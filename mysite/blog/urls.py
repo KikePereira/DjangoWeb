de7075b540a4af023e61faadc5784d50ec2eb21b
Kike <kikepereiraramospr@gmai.com>
2023-01-28 23:29:15 +0100

    DjangoWeb
    a

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list,name='post_list'),
+    path('post/',views.post_list,name='post_list'),
+    path('',views.post,name='post'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+
 ]
